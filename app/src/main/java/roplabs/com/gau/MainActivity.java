906a0de89f43526f7c31772efb6feb321e656e34
Reginald Tan <redge.tan@gmail.com>
2017-03-19 22:44:01 -0400

    first commit

---
+++ app/src/main/java/roplabs/com/gau/MainActivity.java
@@ -0,0 +1,17 @@
+package roplabs.com.gau;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by reg on 2017-03-19.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RssFeedProvider provider;
+    }
+}
